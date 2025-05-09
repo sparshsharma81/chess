922ec00d86b96477aa6d8694a89be8ed33b4b924
sparshsharma81 <81arvindarora@gmail.com>
2025-05-09 13:03:27 +0530

    chess try -3

--- server/server.js
+++ server/server.js
@@ -1,30 +1,28 @@
-import http from 'http';
-import path from 'path';
-import express from 'express';
-import handlebars from 'express-handlebars';
-import socket from 'socket.io';
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const handlebars = require('express-handlebars');
+const socket = require('socket.io');
 
 const config = require('../config');
 
-const myIo = require('./sockets/io'),
-      routes = require('./routes/routes');
+const myIo = require('./sockets/io');
+const routes = require('./routes/routes');
 
-const app = express(),
-      server = http.Server(app),
-      io = socket(server);
-      
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
 
-server.listen(config.port);
-
-games = {};
+global.games = {}; // or pass games to modules
 
 myIo(io);
 
 console.log(`Server listening on port ${config.port}`);
+server.listen(config.port);
 
 const Handlebars = handlebars.create({
-  extname: '.html', 
-  partialsDir: path.join(__dirname, '..', 'front', 'views', 'partials'), 
+  extname: '.html',
+  partialsDir: path.join(__dirname, '..', 'front', 'views', 'partials'),
   defaultLayout: false,
   helpers: {}
 });
@@ -34,4 +32,5 @@ app.set('views', path.join(__dirname, '..', 'front', 'views'));
 app.use('/public', express.static(path.join(__dirname, '..', 'front', 'public')));
 
 routes(app);
+
 module.exports = server;
